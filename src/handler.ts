9b284d6747b7140f1f8abf2c75f6eb9515afa05a
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 22:24:37 +0200

    Log request details

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,9 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
+  console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
+  console.log(`host: ${req.headers.host}`);
+  console.log(`accept: ${req.headers.accept}`);
+  console.log(`user-agent: ${req.headers["user-agent"]}`);
   res.end("Hello World");
 };
