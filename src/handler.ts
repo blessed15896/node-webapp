4453c0956dec1181c54373580b22837b1e18d701
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 20:55:28 +0200

    Refactor using async/await

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,13 @@
 import { readFile } from "fs/promises";
 import { IncomingMessage, ServerResponse } from "http";
 
-export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  readFile("data.json")
-    .then((data: Buffer) => res.end(data, () => console.log("File sent")))
-    .catch((err: Error) => {
-      console.log(`Error: ${err.message}`);
-      res.statusCode = 500;
-      res.end();
-    });
+export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const data: Buffer = await readFile("data.json");
+    res.end(data, () => console.log("File sent"));
+  } catch (err: any) {
+    console.log(`Error: ${err?.message ?? err}`);
+    res.statusCode = 500;
+    res.end();
+  }
 };
