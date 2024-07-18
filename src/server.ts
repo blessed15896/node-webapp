b28c531358fa0c6beacde0f1971ea363621de44c
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 19:43:13 +0200

    Create a simple web application

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { createServer } from "http";
+import { handler } from "./handler";
+
+const port = 5000;
+
+const server = createServer();
+
+server.on("request", handler);
+
+server.listen(port);
+
+server.on("listening", () =>
+  console.log(`(Event) Server listening on port ${port}`)
+);
