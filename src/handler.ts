40a7be3992b9b4fd03a8771a6775631a7ef35651
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 21:21:37 +0200

    Yield control of the main thread using setImmediate

--- src/handler.ts
+++ src/handler.ts
@@ -7,13 +7,18 @@ let shared_counter = 0;
 
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
   const request = shared_counter++;
-  for (let iter = 0; iter < iterations; iter++) {
+  const iterate = async (iter: number = 0) => {
     for (let count = 0; count < total; count++) {
       count++;
     }
     const msg = `Request: ${request}, Iteration: ${iter}`;
     console.log(msg);
     await writePromise.bind(res)(msg + "\n");
-  }
-  await endPromise.bind(res)("Done");
+    if (iter == iterations - 1) {
+      await endPromise.bind(res)("Done");
+    } else {
+      setImmediate(() => iterate(++iter));
+    }
+  };
+  iterate();
 };
