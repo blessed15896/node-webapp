2b804fb792097f7dc4b7ab16c296513f6d69483f
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 20:48:47 +0200

    Refactor handler code to use promises

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,12 @@
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
 import { IncomingMessage, ServerResponse } from "http";
 
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  readFile("data.json", (err: Error | null, data: Buffer) => {
-    if (err == null) {
-      res.end(data, () => console.log("File sent"));
-    } else {
-      console.log(`Error: ${err.message}`);
-      res.statusCode = 500;
-      res.end();
-    }
+  const p: Promise<Buffer> = readFile("data.json");
+  p.then((data: Buffer) => res.end(data, () => console.log("File sent")));
+  p.catch((err: Error) => {
+    console.log(`Error: ${err.message}`);
+    res.statusCode = 500;
+    res.end();
   });
 };
