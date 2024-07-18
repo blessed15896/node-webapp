e939e1ead6d4d6c3501980bb7f9df392d24e465d
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 21:58:05 +0200

    Package worker thread into a callback

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { Worker } from "worker_threads";
 import { endPromise, writePromise } from "./promises";
+import { Count } from "./counter_cb";
 
 const total = 2_000_000_000;
 const iterations = 5;
@@ -8,28 +8,17 @@ let shared_counter = 0;
 
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
   const request = shared_counter++;
-  const worker = new Worker(__dirname + "/count_worker.js", {
-    workerData: {
-      iterations,
-      total,
-      request,
-    },
-  });
-  worker.on("message", async (iter: number) => {
-    const msg = `Request: ${request}, Iteration: ${iter}`;
-    console.log(msg);
-    await writePromise.bind(res)(msg + "\n");
-  });
-  worker.on("exit", async (code: number) => {
-    if (code == 0) await endPromise.bind(res)("Done");
-    else {
+  Count(request, iterations, total, async (err, update) => {
+    if (err !== null) {
+      console.log(err);
       res.statusCode = 500;
       await res.end();
+    } else if (update !== true) {
+      const msg = `Request: ${request}, Iteration: ${update}`;
+      console.log(msg);
+      await writePromise.bind(res)(msg + "\n");
+    } else {
+      await endPromise.bind(res)("Done");
     }
-  });
-  worker.on("error", async (error) => {
-    console.log(error);
-    res.statusCode = 500;
-    await res.end();
   });
 };
