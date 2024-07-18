160490cb1d7784c87e08477c00fe5edb11f36637
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 20:52:31 +0200

    Chain promise methods

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,11 @@ import { readFile } from "fs/promises";
 import { IncomingMessage, ServerResponse } from "http";
 
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  const p: Promise<Buffer> = readFile("data.json");
-  p.then((data: Buffer) => res.end(data, () => console.log("File sent")));
-  p.catch((err: Error) => {
-    console.log(`Error: ${err.message}`);
-    res.statusCode = 500;
-    res.end();
-  });
+  readFile("data.json")
+    .then((data: Buffer) => res.end(data, () => console.log("File sent")))
+    .catch((err: Error) => {
+      console.log(`Error: ${err.message}`);
+      res.statusCode = 500;
+      res.end();
+    });
 };
