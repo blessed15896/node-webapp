3f4bb68da42844ffba3ead7522a64d3c49558a52
Blessed Sibanda <blessed15896@gmail.com>
2024-07-18 21:03:00 +0200

    Promisify `res.end` callback

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,12 @@
 import { readFile } from "fs/promises";
 import { IncomingMessage, ServerResponse } from "http";
+import { endPromise } from "./promises";
 
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
   try {
-    const data: Buffer = await readFile("data.json");
-    res.end(data, () => console.log("File sent"));
+    const data: Buffer = await readFile("package.json");
+    await endPromise.bind(res)(data);
+    console.log("File sent");
   } catch (err: any) {
     console.log(`Error: ${err?.message ?? err}`);
     res.statusCode = 500;
---
+++ src/promises.ts
@@ -0,0 +1,6 @@
+import { ServerResponse } from "http";
+import { promisify } from "util";
+
+export const endPromise = promisify(ServerResponse.prototype.end) as (
+  data: any
+) => Promise<void>;
