6808b37c31a9bd5ccf07f9bc298a045d2fb010ab
Blessed Sibanda <blessed15896@gmail.com>
2024-07-19 11:29:25 +0200

    Use a transformer in object mode for JSON requests

--- src/readHandler.ts
+++ src/readHandler.ts
@@ -5,12 +5,29 @@ export const readHandler = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
-  req.pipe(createLowerTransform()).pipe(res);
+  if (req.headers["content-type"] == "application/json") {
+    req.pipe(createFromJsonTransform()).on("data", (payload) => {
+      if (payload instanceof Array) {
+        res.write(`Received an array with ${payload.length} items`);
+      } else {
+        res.write("Did not receive an array");
+      }
+      res.end();
+    });
+  } else req.pipe(createLowerTransform()).pipe(res);
 };
 
 const createLowerTransform = () =>
   new Transform({
     transform(data, encoding, callback) {
       callback(null, data.toString().toLowerCase());
+    },
+  });
+
+const createFromJsonTransform = () =>
+  new Transform({
+    readableObjectMode: true,
+    transform(data, encoding, callback) {
+      callback(null, JSON.parse(data));
     },
   });
