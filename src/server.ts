7de2e0b1183ab646194c7de2eb9b2b87c2afda8a
Blessed Sibanda <blessed15896@gmail.com>
2024-07-19 08:49:19 +0200

    Handle HTTPs requests

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { createServer } from "http";
+import { createServer as createHttpsServer } from "https";
+import { readFileSync } from "fs";
 import { handler } from "./handler";
 
 const port = 5000;
@@ -7,4 +9,15 @@ const server = createServer(handler);
 
 server.listen(port, () =>
   console.log(`(Event) Server listening on port ${port}`)
+);
+
+const httpsPort = 5500;
+
+const httpsServer = createHttpsServer(
+  { key: readFileSync("key.pem"), cert: readFileSync("cert.pem") },
+  handler
+);
+
+httpsServer.listen(httpsPort, () =>
+  console.log(`HTTPS Server listening on port ${httpsPort}`)
 );
