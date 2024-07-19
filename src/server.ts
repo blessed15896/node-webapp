57d2bde4299e628c04e90ea35a4bdc355857915d
Blessed Sibanda <blessed15896@gmail.com>
2024-07-19 11:45:55 +0200

    Send and download files

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,13 @@ const port = 5000;
 const expressApp: Express = express();
 
 expressApp.post("/read", readHandler);
+
+expressApp.get("/sendcity", (req, res) =>
+  res.sendFile("city.png", { root: "static" })
+);
+
+expressApp.get("/downloadcity", (req, res) => res.download("static/city.png"));
+
 expressApp.use(express.static("static"));
 expressApp.use(express.static("node_modules/bootstrap/dist"));
 
