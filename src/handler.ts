677ecb82284c1be8bcc8c40a34784ec4ec7354d4
Blessed Sibanda <blessed15896@gmail.com>
2024-07-19 08:00:36 +0200

    Parse URL

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,17 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
-  console.log(`host: ${req.headers.host}`);
-  console.log(`accept: ${req.headers.accept}`);
-  console.log(`user-agent: ${req.headers["user-agent"]}`);
+  // console.log(`---- HTTP Method: ${req.method}, URL: ${req.url}`);
+  // console.log(`host: ${req.headers.host}`);
+  // console.log(`accept: ${req.headers.accept}`);
+  // console.log(`user-agent: ${req.headers["user-agent"]}`);
+  const parsedURL = new URL(req.url ?? "", `http://${req.headers.host}`);
+  console.log(`protocol: ${parsedURL.protocol}`);
+  console.log(`hostname: ${parsedURL.hostname}`);
+  console.log(`port: ${parsedURL.port}`);
+  console.log(`pathname: ${parsedURL.pathname}`);
+  parsedURL.searchParams.forEach((val, key) =>
+    console.log(`Search param: ${key}: ${val}`)
+  );
   res.end("Hello World");
 };
