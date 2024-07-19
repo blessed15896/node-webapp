140de5d66558f0ee94512a35cd2e18fbe149b6c5
Blessed Sibanda <blessed15896@gmail.com>
2024-07-19 11:06:15 +0200

    Avoid excessive data buffering

--- src/handler.ts
+++ src/handler.ts
@@ -2,8 +2,23 @@ import { IncomingMessage, ServerResponse } from "http";
 
 export const basicHandler = (req: IncomingMessage, res: ServerResponse) => {
   res.setHeader("Content-Type", "text/plain");
-  for (let i = 0; i < 10; i++) {
-    res.write(`Message: ${i}\n`);
-  }
-  res.end("End");
+  let i = 0;
+  let canWrite = true;
+  let drainCount = 0;
+  const writeData = () => {
+    console.log("Started writing data");
+    do {
+      canWrite = res.write(`Message: ${i++}\n`);
+    } while (i < 10_000 && canWrite);
+    console.log("Buffer is at capacity");
+
+    if (i < 10_000) {
+      res.once("drain", () => {
+        drainCount++;
+        console.log(`Buffer has been drained: (${drainCount})`);
+        writeData();
+      });
+    } else res.end();
+  };
+  writeData();
 };
